5803ef1dfcd96e84c1a1a38afe7734a21bae5925
JasonVolkoff <kashijason@gmail.com>
2021-03-29 06:47:37 -0700

    Updated to better CORS proxy, access request no longer needed

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     function getCoinData(currency, timeframe) {
         console.log("getCoinData Success");
         var baseUrl = "https://api.coinranking.com/v2/coin/" + currency + "?timePeriod=" + timeframe;
-        var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+        var proxyUrl = "https://cors.bridged.cc/";
         var apiKey = "coinrankingc0b595008db85657a50d4082f20ff1ab68d03f2b78445fb8"
         $(`#${timeframe}`).prop("checked", true).css("border", "4px solid green");
         fetch(`${proxyUrl}${baseUrl}`, {
